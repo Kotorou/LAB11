e649dca2bba25868b197c87239a0691a4e7d9018
Kotaro <kota.kawaka@gmail.com_>
2024-12-20 14:47:07 +0700

    lab11.1 finish

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,15 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main(){
+cout << "Press Enter 3 times to reveal your future.";
+cin.ignore();
+cin.ignore();
+cin.ignore();
+string grade[9] ={"A","B+","B","C+","C","D+","D","F","W"};
+srand(time(0));
+int randomnum = rand()%9;
+cout << "You will get "<< grade[randomnum] <<" in this 261102.";
+}
